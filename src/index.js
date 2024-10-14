3bcb85a7ccb5e403c58e1de354bc127c213d29ec
Assaf <assafyechiel@gmail.com>
2024-10-14 20:33:58 +0300

    Add developement mode check

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import "./style.css";
 import { test } from "./home.js";
 
 console.log(test);
+
+if (process.env.NODE_ENV !== "production") {
+    console.log("Looks like we are in development mode!");
+  }
