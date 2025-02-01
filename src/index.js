e8d1c74733cbb6ab71f24850ee138518ad2e3fc0
Assaf <assafyechiel@gmail.com>
2025-02-01 21:53:22 +0200

    Add dist/ directory back to gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import { test } from "./home.js";
-
-console.log(test);
-
-if (process.env.NODE_ENV !== "production") {
-  console.log("Looks like we are in development mode!");
-}
